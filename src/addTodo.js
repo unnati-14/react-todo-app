a60933b518eced573a12abceb551ddc7d20d4a19
MUKESHSIHAG <sihagmukesh22@gmail.com>
2019-01-02 22:38:18 +0530

    todo-app-in-reactjs

---
+++ src/addTodo.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class AddTodo extends Component {
+    state = {
+        content: ''
+    }
+    handleChange = (e) => {
+        this.setState({
+            content: e.target.value
+        })
+    }
+    hadleSubmit = (e) => {
+        e.preventDefault();
+        this.props.addTodo(this.state)
+        this.setState({
+            content: ''
+        })
+    }
+    render(){
+        return(
+            <div>
+                <form onSubmit={ this.hadleSubmit }>
+                    <label>Add todo</label>
+                    <input type="text" onChange={ this.handleChange } value={this.state.content}></input>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default AddTodo;
